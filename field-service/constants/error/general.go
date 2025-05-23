3db068da4c9e57fa620e450e64bbbf3479208809
MarcellinusAditya <marcellinusaditya26@gmail.com>
2025-05-23 17:29:43 +0700

    (field-service):add config, constant,common, and cmd

---
+++ field-service/constants/error/general.go
@@ -0,0 +1,22 @@
+package error
+
+import "errors"
+
+
+
+var (
+	ErrInternalServerError = errors.New("internal server error")
+	ErrSQLError = errors.New("database server failed to execute query")
+	ErrTooManyRequests = errors.New("too many request")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrInvalidToken = errors.New("invalid token")
+	ErrForbidden = errors.New("forbidden")
+)
+
+var GeneralErrors = []error{
+	ErrInternalServerError,
+	ErrSQLError,ErrTooManyRequests,
+	ErrUnauthorized,
+	ErrInvalidToken,
+	ErrForbidden,
+}
