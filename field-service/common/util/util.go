c259bf016c96d6bba5248c63f4fe59f350443a57
MarcellinusAditya <marcellinusaditya26@gmail.com>
2025-05-25 17:28:47 +0700

    (field-service):add func to util

--- field-service/common/util/util.go
+++ field-service/common/util/util.go
@@ -1,14 +1,81 @@
 package util
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math"
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	_ "github.com/spf13/viper/remote"
 )
+
+type PaginationParam struct {
+	Count int64       `json:"count"`
+	Page  int         `json:"page"`
+	Limit int         `json:"limit"`
+	Data  interface{} `json:"data"`
+}
+
+type PaginationResult struct {
+	TotalPage    int         `json:"totalPage"`
+	TotalData    int64       `json:"totalData"`
+	NextPage     *int        `json:"nextPage"`
+	PreviousPage *int        `json:"previousPage"`
+	Page         int         `json:"page"`
+	Limit        int         `json:"limit"`
+	Data         interface{} `json:"data"`
+}
+
+func GeneratePagination(params PaginationParam) PaginationResult {
+	totalPage := int(math.Ceil(float64(params.Count) / float64(params.Limit)))
+
+	var (
+		nextPage     int
+		previousPage int
+	)
+	if params.Page < totalPage {
+		nextPage = params.Page + 1
+	}
+
+	if params.Page > 1 {
+		previousPage = params.Page - 1
+	}
+
+	result := PaginationResult{
+		TotalPage:    totalPage,
+		TotalData:    params.Count,
+		NextPage:     &nextPage,
+		PreviousPage: &previousPage,
+		Page:         params.Page,
+		Limit:        params.Limit,
+		Data:         params.Data,
+	}
+	return result
+}
+
+func GenerateSHA256(inputString string) string {
+	hash := sha256.New()
+	hash.Write([]byte(inputString))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	return hashString
+}
+
+func RupiahFormat(amount *float64) string {
+	stringValue := "0"
+	if amount != nil {
+		humanizeValue := humanize.CommafWithDigits(*amount, 0)
+		stringValue = strings.ReplaceAll(humanizeValue, ",", ".")
+	}
+	return fmt.Sprintf("Rp. %s", stringValue)
+}
 
 func BindFromJSON(dest any, filename, path string) error {
 	v := viper.New()
@@ -18,12 +85,12 @@ func BindFromJSON(dest any, filename, path string) error {
 	v.SetConfigName(filename)
 
 	err := v.ReadInConfig()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	err = v.Unmarshal(&dest)
-	if err !=nil{
+	if err != nil {
 		logrus.Errorf("failed to unmarshal: %v", err)
 		return err
 	}
@@ -31,21 +98,22 @@ func BindFromJSON(dest any, filename, path string) error {
 	return nil
 }
 
-func SetEnvFromConsulKV(v *viper.Viper)error{
+func SetEnvFromConsulKV(v *viper.Viper) error {
 	env := make(map[string]any)
 
-	err:= v.Unmarshal(&env)
-	if err != nil{
+	err := v.Unmarshal(&env)
+	if err != nil {
 		logrus.Errorf("failed to unmarshal: %v", err)
 		return err
 	}
-	for k,v := range env{
-		var(
+
+	for k, v := range env {
+		var (
 			valOf = reflect.ValueOf(v)
-			val string
+			val   string
 		)
 
-		switch valOf.Kind(){
+		switch valOf.Kind() {
 		case reflect.String:
 			val = valOf.String()
 		case reflect.Int:
@@ -60,8 +128,8 @@ func SetEnvFromConsulKV(v *viper.Viper)error{
 			val = strconv.FormatBool(valOf.Bool())
 		}
 
-		err = os.Setenv(k,val)
-		if err !=nil{
+		err = os.Setenv(k, val)
+		if err != nil {
 			logrus.Errorf("failed to set env: %v", err)
 			return err
 		}
@@ -70,30 +138,30 @@ func SetEnvFromConsulKV(v *viper.Viper)error{
 	return nil
 }
 
-func BindFromConsul(dest any, endPoint, path string)error{
+func BindFromConsul(dest any, endPoint, path string) error {
 	v := viper.New()
-
 	v.SetConfigType("json")
-	err := v.AddRemoteProvider("consule", endPoint, path)
-	if err !=nil{
-		logrus.Errorf("failed to add remote provide: %v", err)
+	err := v.AddRemoteProvider("consul", endPoint, path)
+	if err != nil {
+		logrus.Errorf("failed to add remote provider: %v", err)
 		return err
 	}
 
 	err = v.ReadRemoteConfig()
-	if err != nil{
+	if err != nil {
 		logrus.Errorf("failed to read remote config: %v", err)
 		return err
 	}
+
 	err = v.Unmarshal(&dest)
-	if err != nil{
-		logrus.Errorf("failed to read unmarshal: %v", err)
+	if err != nil {
+		logrus.Errorf("failed to unmarshal: %v", err)
 		return err
 	}
 
-	err=SetEnvFromConsulKV(v)
-	if err != nil{
-		logrus.Errorf("failed to set env consul kv : %v", err)
+	err = SetEnvFromConsulKV(v)
+	if err != nil {
+		logrus.Errorf("failed to set env from consul kv: %v", err)
 		return err
 	}
 
