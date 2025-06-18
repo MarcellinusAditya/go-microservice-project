994798602f0173dc08da8840a2b69aafb073ae47
MarcellinusAditya <marcellinusaditya26@gmail.com>
2025-06-18 16:49:20 +0700

    (order-service):add services

--- order-service/clients/user/user.go
+++ order-service/clients/user/user.go
@@ -9,6 +9,8 @@ import (
 	config2 "order-service/config"
 	"order-service/constants"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type UserClient struct {
@@ -17,6 +19,7 @@ type UserClient struct {
 
 type IUserClient interface {
 	GetUserByToken(context.Context) (*UserData, error)
+	GetUserByUUID(context.Context, uuid.UUID) (*UserData, error)
 }
 
 func NewUserClient(client config.IClientConfig) IUserClient {
@@ -41,6 +44,37 @@ func (u *UserClient) GetUserByToken(ctx context.Context) (*UserData, error) {
 		Set(constants.XApiKey, apiKey).
 		Set(constants.XRequestAt, fmt.Sprintf("%d", unixTime)).
 		Get(fmt.Sprintf("%s/api/v1/auth/user", u.client.BaseURL()))
+
+	resp, _, errs := request.EndStruct(&response)
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("user response: %s", response.Message)
+	}
+
+	return &response.Data, nil
+}
+
+func (u *UserClient) GetUserByUUID(ctx context.Context, uuid uuid.UUID) (*UserData, error) {
+	unixTime := time.Now().Unix()
+	generateAPIKey := fmt.Sprintf("%s:%s:%d",
+		config2.Config.AppName,
+		u.client.SignatureKey(),
+		unixTime,
+	)
+	apiKey := util.GenerateSHA256(generateAPIKey)
+	token := ctx.Value(constants.Token).(string)
+	bearerToken := fmt.Sprintf("Bearer %s", token)
+
+	var response UserResponse
+	request := u.client.Client().Clone().
+		Set(constants.Authorization, bearerToken).
+		Set(constants.XServiceName, config2.Config.AppName).
+		Set(constants.XApiKey, apiKey).
+		Set(constants.XRequestAt, fmt.Sprintf("%d", unixTime)).
+		Get(fmt.Sprintf("%s/api/v1/auth/%s", u.client.BaseURL(), uuid))
 
 	resp, _, errs := request.EndStruct(&response)
 	if len(errs) > 0 {
